bb9083f6f97392750166cd9b87e5ce7ed9af83d9
Sébastien Kalinine <sebastien@diplomeo.fr>
2016-05-23 01:34:43 +0200

    Enable default cakephp routes

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -210,3 +210,9 @@
  * how to customize the loading of plugin routes.
  */
   CakePlugin::routes();
+
+/**
+ * Load the CakePHP default routes. Only remove this if you do not want to use
+ * the built-in default routes.
+ */
+  require CAKE . 'Config' . DS . 'routes.php';
