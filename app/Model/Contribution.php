b20a4b1d213c4cad47481c44ef1234258c3f8645
Sébastien Kalinine <sebastien@diplomeo.fr>
2016-06-13 21:27:43 +0200

    Same

--- app/Model/Contribution.php
+++ app/Model/Contribution.php
@@ -81,7 +81,7 @@ class Contribution extends AppModel {
       )
     ));
 var_dump($this->data[$this->alias]['user_id']);
-var_dump($user);
+var_dump($user['User']);
 exit();
     // Mail administrateur
     $Email = new CakeEmail('adminNewContrib');
